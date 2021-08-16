05c71994c4d0f3137887b583cd8defa0f251dae8
NicolasFigueredoo <86845893+NicolasFigueredoo@users.noreply.github.com>
2021-08-16 15:15:35 -0300

    Revert "add files"
    This reverts commit b8290fd6613f2aa7a30c5cf79b2aec7de36233e8.

--- assets/js/storage.js
+++
@@ -1,8 +0,0 @@
-  
-function obtenerUsuariosDeStorage(){
-    let listadeusuariosstorage = [];
-    if(localStorage.getItem("usuarios") != null){
-        listadeusuariosstorage = JSON.parse(localStorage.getItem("usuarios"));
-    }
-    return listadeusuariosstorage;
-}
